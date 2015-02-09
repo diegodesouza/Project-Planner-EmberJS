e8301b3fee9e88c5b3833ce50e7918651c9597d9
Diego De Souza <diegotrus7@gmail.com>
2015-02-08 21:45:29 -0500

    created frontend files and config

--- backend/db/migrate/20150209001535_create_projects.rb
+++ backend/db/migrate/20150209001535_create_projects.rb
@@ -3,10 +3,9 @@ class CreateProjects < ActiveRecord::Migration
     create_table :projects do |t|
       t.string :name, null: false
       t.text :description
-
       t.timestamps null: false
-
-      add_index :projects, :name, unique: true
     end
+    
+    add_index :projects, :name, unique: true
   end
 end
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('projects', function() {
+    this.route('show', { path: ':project_id' });
+  });
 });
 
 export default Router;
